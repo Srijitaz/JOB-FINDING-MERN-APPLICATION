43953ccf3f527b6ca61117e6e78bc729f0a12bfb
SRIJITA SAHA <116731752+Srijitaz@users.noreply.github.com>
2024-07-14 19:50:58 +0530

    Update MyJobs.jsx

--- frontend/src/components/Job/MyJobs.jsx
+++ frontend/src/components/Job/MyJobs.jsx
@@ -17,7 +17,7 @@ const MyJobs = () => {
     const fetchJobs = async () => {
       try {
         const { data } = await axios.get(
-          "https://job-seeking-mern-deployment.onrender.com/api/v1/job/getmyjobs",
+          "http://localhost:4000/api/v1/job/getmyjobs",
           { withCredentials: true }
         );
         setMyJobs(data.myJobs);
